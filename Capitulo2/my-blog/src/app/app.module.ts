350943480ecb1aeacff1a4f6da2b35ab45114119
Daniel Bruno de Andrade Pereira <dabranpe@gmail.com>
2024-05-03 17:11:44 -0300

    Continuação capítulo 2

--- Capitulo2/my-blog/src/app/app.module.ts
+++ Capitulo2/my-blog/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ContactModule } from './contact/contact.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ContactModule
   ],
   providers: [
     provideClientHydration()
